a1f5cf6914097e3b4bfc67172202c671a5a8538d
lyl <sky.liang2000@gmail.com>
2018-10-30 15:45:35 -0400

    Expertus challenge

---
+++ src/main/java/com/example/challenge/Client.java
@@ -0,0 +1,46 @@
+package com.example.challenge;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String firstName){
+        this.lastName = lastName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getTelephone(){
+        return telephone;
+    }
+    public void setTelephone(String telephone){
+        this.telephone = telephone;
+    }
+}
